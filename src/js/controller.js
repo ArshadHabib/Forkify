f97b582c802f6c7f406334e1f1c522dedce414d6
Arshad Habib <arshad4850@gmail.com>
2023-06-15 00:53:18 +0500

    8 Bookmarks: used local storage for bookmarks, refactored bookmarks and results view code into single file

--- src/js/controller.js
+++ src/js/controller.js
@@ -3,10 +3,12 @@ import recipeView from './views/recipeView.js';
 import searchView from './views/searchView.js';
 import resultsView from './views/resultsView.js';
 import paginationView from './views/paginationView.js';
+import bookmarksView from './views/bookmarksView.js';
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 import { async } from 'regenerator-runtime/runtime';
 import paginationView from './views/paginationView.js';
+import bookmarksView from './views/bookmarksView.js';
 
 const controlRecipes = async function () {
   try {
@@ -20,9 +22,13 @@ const controlRecipes = async function () {
     // update results view to mark selected search result
     resultsView.update(model.getSearchResultsPage());
 
+    // updating bookmarks view
+    bookmarksView.update(model.state.bookmarks);
+
     //2 Rendering recipe
     recipeView.render(model.state.recipe);
   } catch (err) {
+    console.log(err);
     recipeView.renderError();
   }
 };
@@ -55,12 +61,23 @@ const controlServings = function (newServings) {
 };
 
 const controlAddBookmark = function () {
+  // Add/remove bookmark
   if (!model.state.recipe.bookmarked) model.addBookmark(model.state.recipe);
   else model.deleteBookmark(model.state.recipe.id);
+
+  //update recipe view
   recipeView.update(model.state.recipe);
+
+  //render bookmarks
+  bookmarksView.render(model.state.bookmarks);
+};
+
+const controlBookmarks = function () {
+  bookmarksView.render(model.state.bookmarks);
 };
 
 const init = function () {
+  bookmarksView.addHandlerRender(controlBookmarks);
   recipeView.addHandlerRender(controlRecipes);
   recipeView.addHandlerUpdateServings(controlServings);
   searchView.addHandlerSearch(controlSearchResults);
