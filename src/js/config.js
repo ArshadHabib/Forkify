53affd5e64fb57dd43488a03dcf6fa6e564059fe
Arshad Habib <arshad4850@gmail.com>
2023-06-15 19:32:30 +0500

    9 Uploading Recipe: created API request method and rendering the newly created recipe

--- src/js/config.js
+++ src/js/config.js
@@ -1,3 +1,5 @@
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
+export const KEY = 'bacb1779-b2cc-42f1-88a3-276c0feb0088';
+export const MODAL_CLOSE_SEC = 2.5;
