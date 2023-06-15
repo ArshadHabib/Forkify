3f4b4cb652e501785bc5c7aa271e2693a28cf1dd
Arshad Habib <arshad4850@gmail.com>
2023-06-15 22:05:15 +0500

    10 Bug Parcel Build: solved the parcel build error by not hoisting scope

--- src/js/controller.js
+++ src/js/controller.js
@@ -8,7 +8,6 @@ import bookmarksView from './views/bookmarksView.js';
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 import { async } from 'regenerator-runtime/runtime';
-import paginationView from './views/paginationView.js';
 import bookmarksView from './views/bookmarksView.js';
 import addRecipeView from './views/addRecipeView.js';
 
